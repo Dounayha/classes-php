7d14f801ad003b78b76f773c69eb799c2e7dcde0
Dounayha <dounayha05@gmail.com>
2024-08-26 11:30:05 +0200

    reinitialise les attributs

--- User.php
+++ User.php
@@ -5,5 +5,14 @@ public $login;
 public $email;
 public $firstname;
 public $lastname;
+
+public function __construct($login, $email, $firstname, $lastname, $id = null) {
+    $this->id = $id;
+    $this->login = $login;
+    $this->email = $email;
+    $this->firstname = $firstname;
+    $this->lastname = $lastname;
+}
+
 }
 
