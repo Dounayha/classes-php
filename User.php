294ee9dd51a424e8d354c7a7b94bbd2c3c8fb58e
Dounayha <dounayha05@gmail.com>
2024-08-26 11:35:40 +0200

    creer un nvx utilisateur

--- User.php
+++ User.php
@@ -13,6 +13,17 @@ public function __construct($login, $email, $firstname, $lastname, $id = null) {
     $this->firstname = $firstname;
     $this->lastname = $lastname;
 }
-
+//creer un nvx utilisateur
+public function create($mysqli) {
+    $sql = "INSERT INTO users (login, email, firstname, lastname) VALUES (?, ?, ?, ?)";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("ssss", $this->login, $this->email, $this->firstname, $this->lastname);
+    if ($stmt->execute()) {
+        $this->id = $stmt->insert_id;
+        return true;
+    } else {
+        return false;
+    }
+}
 }
 
